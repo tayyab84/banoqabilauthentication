663fa7a9698471a5639744b217cc6a42cdb0df96
tayyab84 <160376543+tayyab84@users.noreply.github.com>
2024-02-22 21:57:18 +0500

    Add files via upload

---
+++ app-2.js
@@ -0,0 +1,28 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwS9kmIxLzz6oev-Xzim3BO7HNhirrJ5s",
+  authDomain: "authform-edb08.firebaseapp.com",
+  databaseURL: "https://authform-edb08-default-rtdb.firebaseio.com",
+  projectId: "authform-edb08",
+  storageBucket: "authform-edb08.appspot.com",
+  messagingSenderId: "565111279275",
+  appId: "1:565111279275:web:42be7c917c792f3ad9b1ee"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth()
+
+ console.log(auth)
+
+
+
+let signOutButton = document.getElementById("signout")
+signOutButton.addEventListener("click", (e) => {
+  //Prevent Default Form Submission Behavior
+  e.preventDefault()
+  console.log("clicked")
+  
+  auth.signOut()
+  alert("Signed Out")
+  window.location = "index.html";
+})
